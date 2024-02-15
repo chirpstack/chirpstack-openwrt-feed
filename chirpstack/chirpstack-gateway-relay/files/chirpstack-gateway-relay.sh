59b4e3023475693a5a724d5932742a72c9a8bcd2
Orne Brocaar <info@brocaar.com>
2024-02-15 10:56:12 +0000

    gateway-relay: Add Gateway Relay package.

---
+++ chirpstack/chirpstack-gateway-relay/files/chirpstack-gateway-relay.sh
@@ -0,0 +1,114 @@
+. /lib/functions.sh
+
+configure() {
+	local config_name="$1"
+	mkdir -p /var/etc/$config_name
+	config_load "$config_name"
+
+	config_foreach conf_rule_global "global" "$config_name"
+	config_foreach conf_rule_relay "relay" "$config_name"
+	config_foreach conf_rule_relay_data_rate "relay_data_rate" "$config_name"
+	config_foreach conf_rule_backend_concentratord "backend_concentratord" "$config_name"
+	config_foreach conf_rule_backend_relay_concentratord "backend_relay_concentratord" "$config_name"
+}
+
+conf_rule_global() {
+	local cfg="$1"
+	local config_name="$2"
+	local region
+
+	config_get region $cfg region
+
+	cp /etc/chirpstack-gateway-relay/region_$region.toml /var/etc/$config_name/region.toml
+	cat > /var/etc/$config_name/chirpstack-gateway-relay.toml <<- EOF
+		[logging]
+			log_level="INFO"
+			log_to_syslog=true
+	EOF
+}
+
+conf_rule_relay() {
+	local cfg="$1"
+	local config_name="$2"
+	local border_gateway tx_power
+
+	config_get_bool border_gateway $cfg border_gateway
+	config_get tx_power $cfg tx_power
+
+	if [ "$border_gateway" = "1" ]; then
+		border_gateway="true"
+	else
+		border_gateway="false"
+	fi
+
+	cat >> /var/etc/$config_name/chirpstack-gateway-relay.toml <<- EOF
+		[relay]
+			border_gateway=$border_gateway
+			tx_power=$tx_power
+			frequencies=[
+	EOF
+
+	config_list_foreach $cfg frequency conf_rule_frequency "$config_name"
+
+	cat >> /var/etc/$config_name/chirpstack-gateway-relay.toml <<- EOF
+			]
+	EOF
+}
+
+conf_rule_frequency() {
+	local config_name="$2"
+	cat >> /var/etc/$config_name/chirpstack-gateway-relay.toml <<- EOF
+		$1,
+	EOF
+}
+
+conf_rule_relay_data_rate() {
+	local cfg="$1"
+	local config_name="$2"
+	local modulation spreading_factor bandwidth code_rate bitrate
+
+	config_get modulation $cfg modulation
+	config_get spreading_factor $cfg spreading_factor
+	config_get bandwidth $cfg bandwidth
+	config_get code_rate $cfg code_rate
+	config_get bitrate $cfg bitrate
+
+	cat >> /var/etc/$config_name/chirpstack-gateway-relay.toml <<- EOF
+		[relay.data_rate]
+			modulation="$modulation"
+			spreading_factor=$spreading_factor
+			bandwidth=$bandwidth
+			code_rate="$code_rate"
+			bitrate=$bitrate
+	EOF
+}
+
+conf_rule_backend_concentratord() {
+	local cfg="$1"
+	local config_name="$2"
+	local event_url command_url
+
+	config_get event_url $cfg event_url
+	config_get command_url $cfg command_url
+
+	cat >> /var/etc/$config_name/chirpstack-gateway-relay.toml <<- EOF
+		[backend.concentratord]
+			event_url="$event_url"
+			command_url="$command_url"
+	EOF
+}
+
+conf_rule_backend_relay_concentratord() {
+	local cfg="$1"
+	local config_name="$2"
+	local event_url command_url
+
+	config_get event_url $cfg event_url
+	config_get command_url $cfg command_url
+
+	cat >> /var/etc/$config_name/chirpstack-gateway-relay.toml <<- EOF
+		[backend.relay_concentratord]
+			event_url="$event_url"
+			command_url="$command_url"
+	EOF
+}
---
+++ chirpstack/luci-app-chirpstack-gateway-relay/htdocs/luci-static/resources/view/chirpstack/chirpstack-gateway-relay.js
@@ -0,0 +1,91 @@
+'use strict';
+'require view';
+'require form';
+'require uci';
+
+return view.extend({
+    render: function () {
+        var m, s, o, ro, as;
+
+        m = new form.Map('chirpstack-gateway-relay', _('ChirpStack Gateway Relay'), _('ChirpStack Gateway Relay turns a LoRa gateway into a Relay.'));
+        m.tabbed = true;
+
+        s = m.section(form.TypedSection, 'global', _('Global configuration'));
+        s.anonymous = true;
+
+        // Is enabled
+        s.option(form.Flag, 'enabled', _('Enabled'), _('Enable ChirpStack Gateway Relay'));
+
+        // Region
+        o = s.option(form.ListValue, 'region', _('Region'));
+        o.value('eu868', 'EU868');
+
+        s = m.section(form.TypedSection, 'relay', _('Relay configuration'));
+        s.anonymous = true;
+
+        // Is border gateway
+        s.option(form.Flag, 'border_gateway', _('Border gateway'), _('Is Border Gateway'), _('A Border Gateway is connected to the internet and forwards received data to ChirpStack'));
+
+        // Frequencies
+        o = s.option(form.DynamicList, 'frequency', _('Frequency'), _('The ChirpStack Gateway Relay will randomly use one of the configured frequencies'));
+        o.datatype = 'integer'
+
+        s = m.section(form.TypedSection, 'relay_data_rate', _('Relay data-rate configuration'));
+        s.anonymous = true;
+
+        // Modulation
+        o = s.option(form.ListValue, 'modulation', _('Modulation'));
+        o.value('LORA', 'LoRa');
+        o.value('FSK', 'FSK');
+
+        // Spreading-factor
+        o = s.option(form.Value, 'spreading_factor', _('Spreading-factor (LoRa)'));
+        o.datatype = 'integer';
+
+        // Bandwidth
+        o = s.option(form.Value, 'bandwidth', _('Bandwidth'));
+        o.datatype = 'integer';
+
+        // Code-rate
+        o = s.option(form.ListValue, 'code_rate', _('Code-rate (LoRa)'));
+        o.value('4/5', '4/5 (sub-GHz)');
+        o.value('4/8', '4/8 (2.4 GHz)');
+
+        // Bitrate
+        o = s.option(form.Value, 'bitrate', _('Bitrate (FSK)'));
+        o.datatype = 'integer';
+
+        s = m.section(form.TypedSection, 'backend_concentratord', _('Backend configuration'));
+        s.anonymous = true;
+
+        // Event url
+        o = s.option(form.ListValue, 'event_url', _('Event URL'));
+        o.value('ipc:///tmp/concentratord_event', 'ipc:///tmp/concentratord_event');
+        o.value('ipc:///tmp/concentratord_slot1_event', 'ipc:///tmp/concentratord_slot1_event');
+        o.value('ipc:///tmp/concentratord_slot2_event', 'ipc:///tmp/concentratord_slot2_event');
+
+        // Command url
+        o = s.option(form.ListValue, 'command_url', _('Command URL'));
+        o.value('ipc:///tmp/concentratord_command', 'ipc:///tmp/concentratord_command');
+        o.value('ipc:///tmp/concentratord_slot1_command', 'ipc:///tmp/concentratord_slot1_command');
+        o.value('ipc:///tmp/concentratord_slot2_command', 'ipc:///tmp/concentratord_slot2_command');
+
+        s = m.section(form.TypedSection, 'backend_relay_concentratord', _('Relay Backend configuration'));
+        s.anonymous = true;
+
+        // Event url
+        o = s.option(form.ListValue, 'event_url', _('Event URL'));
+        o.value('ipc:///tmp/concentratord_event', 'ipc:///tmp/concentratord_event');
+        o.value('ipc:///tmp/concentratord_slot1_event', 'ipc:///tmp/concentratord_slot1_event');
+        o.value('ipc:///tmp/concentratord_slot2_event', 'ipc:///tmp/concentratord_slot2_event');
+
+        // Command url
+        o = s.option(form.ListValue, 'command_url', _('Command URL'));
+        o.value('ipc:///tmp/concentratord_command', 'ipc:///tmp/concentratord_command');
+        o.value('ipc:///tmp/concentratord_slot1_command', 'ipc:///tmp/concentratord_slot1_command');
+        o.value('ipc:///tmp/concentratord_slot2_command', 'ipc:///tmp/concentratord_slot2_command');
+
+
+        return m.render();
+    }
+});
