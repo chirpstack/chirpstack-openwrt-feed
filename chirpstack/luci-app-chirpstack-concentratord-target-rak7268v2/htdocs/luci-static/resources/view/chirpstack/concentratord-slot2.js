c5be6464abe54e2a9a7cea049bce8653d428272c
Xose Pérez <xose.perez@gmail.com>
2024-10-08 17:19:21 +0200

    Revert to using RAK2287 for slot2 on RAK7268v2

--- chirpstack/luci-app-chirpstack-concentratord-target-rak7268v2/htdocs/luci-static/resources/view/chirpstack/concentratord-slot2.js
+++ chirpstack/luci-app-chirpstack-concentratord-target-rak7268v2/htdocs/luci-static/resources/view/chirpstack/concentratord-slot2.js
@@ -15,8 +15,8 @@ return view.extend({
                     name: "SX1302 / SX1303",
                     shields: [
                         {
-                            id: "rak_5146",
-                            name: "RAK - RAK5146",
+                            id: "rak_2287",
+                            name: "RAK - RAK2287",
                             supportedRegions: ["AS923", "AS923_2", "AS923_3", "AS923_4", "AU915", "CN470", "EU433", "EU868", "IN865", "KR920", "RU864", "US915"],
                             defaultFlags: {
                                 usb: true,
